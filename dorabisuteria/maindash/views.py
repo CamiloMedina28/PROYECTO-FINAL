0c4c8fb30519a040d2e32a4516e2b3ab519c58db
CamiloMedina28 <medinacamilo2829@gmail.com>
2024-03-26 16:38:47 -0500

    CONFIGURACIONES DE BASE_DE_DATOS. creación del modelo relacional en workbench y creación del script sql para la generación de las tablas que se modelaron en el modelo relacional. SE DEBE PLANTEAR COMO FUNCIONARÁ LA SUBIDA DE LAS IMAGENES DE LOS PRODUCTOS A LA BASE DE DATOS Y SE DEBEN GENERAR LAS RESTRICCIONES ON UPDATE Y ON DELETE DE LAS LLAVES FORÁNEAS QUE FUERON GENERADAS.

--- dorabisuteria/maindash/views.py
+++ dorabisuteria/maindash/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+import mysql.connector as db_connector
 
 # Create your views here.
 
 def index(request):
     return render (request, "main_dash/index.html")
+
+def register_form_data(request):
+    db_reg_info = db_connector.connect(
+        host = "dorabisuteriadb.mysql.database.azure.com",
+        user = "",
+        password =""
+    )
+
+
+
+
+
